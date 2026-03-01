95ce35fca3996dd1d36c3b4e99ff0f4c8ba573c3
Lothaka <louiskaiser67@gmail.com>
2026-03-01 01:20:30 +0100

    fonction sauvergarde

---
+++ Grille/sauvegarde.py
@@ -0,0 +1,31 @@
+import json 
+
+fichier_sauvergarde = "Sauvegardes/grilles_jouees.json"
+
+def sauvegarde (grille, temps, difficulte, score):
+    
+    #On charge l'historique si elle existe ou on le crée  
+    lecture=open(fichier_sauvergarde, "r")
+    liste_parties = json.load(lecture)
+    lecture.close()
+    
+    #Les données de la partie qu'on va sauvegarder 
+    nouvelle_partie={
+        "difficulte" : difficulte,
+        "grille" : grille,
+        "temps" : temps,
+        "score" : score,
+    }
+
+    #On ajoute notre nouvelle partie au début de la pile
+    liste_parties.insert(0,nouvelle_partie)
+
+    #On garde les 100 dernieres parties 
+    liste_parties=liste_parties[:100]
+
+    #On sauvegarde notre fichier 
+    ecriture = open(fichier_sauvergarde,"w")
+    json.dump(liste_parties,ecriture,indent=4)
+    ecriture.close()
+
+    return 
